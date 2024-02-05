d79fb69d2d883f27ef8ab5a7f3e499b2c56205af
HarryZ <uqbarz@gmail.com>
2024-02-05 23:33:15 +0800

    cancel prebundle and cache

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,8 @@
 import { View, Text } from '@tarojs/components'
 import Taro, { useLoad } from '@tarojs/taro'
 import './index.scss'
+import React, {useState} from 'react'
+import { CalendarCard } from '@nutui/nutui-react-taro'
 
 export default function Index() {
 
@@ -9,21 +11,38 @@ export default function Index() {
     const res = Taro.cloud.callContainer({
       config: {
         env: 'prod-7gbkokc9486b1064',
-      },
-      path: '/',
-      method: 'GET',
-      header: {
-        'X-WX-SERVICE': 'mybackend',
-      },
-    });
-    console.log(res)
+        },
+        path: '/',
+        method: 'GET',
+        header: {
+          'X-WX-SERVICE': 'mybackend',
+        },
+      });
+      console.log(res)
 
-    console.log('Page loaded.')
-  })
+      console.log('Page loaded.')
+    })
+
+    const renderDayTop = (day) => {
+      return day.date === 8 ? '☺' : ''
+    }
+    const renderDay = (day) => {
+      return day.date <= 9 ? `0${day.date}` : day.date
+    }
+    const renderDayBottom = (day) => {
+      return day.date === 8 ? '节日' : ''
+    }
 
   return (
-    <View className='index'>
-      <Text>Hello world!</Text>
-    </View>
+    <div>
+      <View className='index'>
+        <Text>Hello world!</Text>
+      </View>
+      <CalendarCard
+        renderDayTop={renderDayTop}
+        renderDay={renderDay}
+        renderDayBottom={renderDayBottom}
+      />
+    </div>
   )
 }
