3949da0a10a1239d07eca8df18656be5721cbe50
HarryZ <uqbarz@gmail.com>
2024-02-05 23:17:25 +0800

    add tabbar icons

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,40 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/new/new',
+    'pages/statistics/statistics',
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
-  }
+  },
+  tabBar: {
+    color: '#000',
+    selectedColor: '#56abe4',
+    backgroundColor: '#fff',
+    borderStyle: 'black',
+    list: [
+      {
+        pagePath: 'pages/new/new',
+        text: '新建',
+        iconPath: 'assets/icon/new.png',
+        selectedIconPath: 'assets/icon/new-active.png',
+      },
+      {
+        pagePath: 'pages/index/index',
+        text: '主页',
+        iconPath: 'assets/icon/home.png',
+        selectedIconPath: 'assets/icon/home-active.png'
+      },
+      {
+        pagePath: 'pages/statistics/statistics',
+        text: '统计',
+        iconPath: 'assets/icon/palette.png',
+        selectedIconPath: 'assets/icon/palette-active.png'
+      },
+    ]
+  },
+
 })
