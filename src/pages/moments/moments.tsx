30bc1d9fe1e1ae831391bdc880a63a3a358721ec
HarryZ <uqbarz@gmail.com>
2024-02-07 18:00:25 +0800

    add notecard

---
+++ src/pages/moments/moments.tsx
@@ -0,0 +1,10 @@
+import NoteCard from "@/components/notecard/notecard"
+import './moments.scss'
+
+export default function Moments() {
+    return (
+        <div className='moments'>
+            <NoteCard />
+        </div>
+    )
+}
